ea29a03186e27b4fdf3f4fcfb9814e6acbaded16
tohid820 GitHub <tohidkhan896.tk@gmail.com>
2023-04-13 15:56:29 +0530

    first commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static("Public"));
 app.use(bodyparser.urlencoded({extended:true}));
 
 app.get("/", function(req,res){
-  res.sendFile(__dirname + "/signup.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/",function(req,res){
